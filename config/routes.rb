477ea3411b02088f1732c6b259621a89b6013498
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-25 11:37:14 +0000

    Added facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   resources :restaurants do
     resources :reviews
   end
+
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => destroy_user_session
+  # end
 
   get 'restaurants' => 'restaurants#index'
 
