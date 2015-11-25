48fb5fa4b3858f5f429aa93a87d3af7bcf22ba9d
Ezzy Elliott <ezzy.elliott@gmail.com>
2015-11-25 14:14:10 +0000

    Added user association to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125102502) do
+ActiveRecord::Schema.define(version: 20151125141215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20151125102502) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "description"
+    t.integer  "user_id"
   end
+
+  add_index "restaurants", ["user_id"], name: "index_restaurants_on_user_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.text     "thoughts"
@@ -33,6 +36,11 @@ ActiveRecord::Schema.define(version: 20151125102502) do
   end
 
   add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
+
+  create_table "students", id: false, force: :cascade do |t|
+    t.integer "id"
+    t.string  "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -56,5 +64,6 @@ ActiveRecord::Schema.define(version: 20151125102502) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
+  add_foreign_key "restaurants", "users"
   add_foreign_key "reviews", "restaurants"
 end
