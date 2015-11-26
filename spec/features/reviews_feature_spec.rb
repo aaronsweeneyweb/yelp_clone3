632fee9139dc7dae98718bcd91dcccd256489b55
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-11-26 15:03:12 +0000

    Tests for average rating (failing)

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -13,6 +13,7 @@ feature 'Reviewing' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content "So so"
   end
+
   scenario 'cannot add two reviews' do
     visit '/restaurants'
     click_link 'Review KFC'
@@ -27,5 +28,19 @@ feature 'Reviewing' do
     click_button 'Leave Review'
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content "You have already reviewed this restaurant"
+  end
+
+  def leave_review(thoughts, rating)
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: thoughts
+    select rating, from: 'Rating'
+    click_button 'Leave Review'
+  end
+
+  scenario 'displays an everage rating for all reviews' do
+    leave_review('So so', '3')
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
   end
 end
