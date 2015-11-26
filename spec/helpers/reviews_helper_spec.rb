aa31421a9863e87c113786845082dc78b635a40c
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2015-11-26 16:46:56 +0000

    Star rating tests failing

--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -10,11 +10,14 @@ require 'rails_helper'
 #     end
 #   end
 # end
-describe ReviewsHelper, type: :helper do
-  # pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe ReviewsHelper, :type => :helper do
   context '#star_rating' do
     it 'does nothing for not a number' do
       expect(helper.star_rating('N/A')).to eq('N/A')
+    end
+
+    it 'returns five black stars for five' do
+      expect(helper.star_rating(5)).to eq '★★★★★'
     end
   end
 end
